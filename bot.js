a1aa677254e89ae39c5284b49b2edd7e59f94007
David Poindexter <drpoindexter@gmail.com>
2019-03-28 12:40:57 -0400

    Initial commit

--- bot.js
+++ bot.js
@@ -1,3 +1,76 @@
+const tmi = require('tmi.js');
 const token = require('./auth');
 
-console.log(`Token is: ${token.oauth}`);
+const opts = {
+  identity: {
+    username: 'roberttables',
+    password: `${token.OAUTH_TOKEN}`
+  },
+  channels: ['roberttables']
+};
+
+// Create a client
+const client = new tmi.client(opts);
+
+// Register the event handlers
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+
+// Connect to The Twitches
+client.connect();
+
+// Define onMessageHandler
+function onMessageHandler(target, context, msg, self) {
+  // console.log(target);
+  if (self) {
+    // Ignore messages form the bot itself
+    return;
+  }
+  // Remove the whitespace (ex: !dice<space> !dice)
+  const commandName = msg.trim();
+
+  // console.log(JSON.stringify(context, 0, 4));
+  const roll = roll20();
+  let spice = '';
+  // Switch on a known command
+  if (commandName === '!dice') {
+    switch (roll) {
+      case 20:
+        spice = ` - NAT 20 robert68Hecc`;
+        break;
+      case 1:
+        spice = ` - NAT 1, you lose the blanket robert68Hecc`;
+        break;
+      default:
+        break;
+    }
+    client.say(
+      target,
+      `@${context.username} You rolled for initiative: ${roll}${spice}`
+    );
+  }
+  const commandMap = {
+    '!dice': dice,
+    '!coin': flipCoin
+  };
+
+  commandMap[commandName]();
+}
+// Define onConnectHandler
+function onConnectedHandler(addr, port) {
+  client.say(
+    `${opts.channels[0]}`,
+    `robert68Hecc Bot in the chat ready to roll! robert68Shipit`
+  );
+}
+
+// Callback for function
+function roll20() {
+  const sides = 20;
+  return Math.floor(Math.random() * sides) + 1;
+}
+
+// Flip Coin
+function flipCoin() {
+  return Math.floor(Math.random() * 2) + 1;
+}
